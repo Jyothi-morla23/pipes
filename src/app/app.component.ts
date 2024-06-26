33ba12e42ae7a5eed53047bfbfd975637eb77b2a
jyothi-morla23 <jyothim2303@gmail.com>
2024-06-26 22:23:38 +0530

    bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  public name: string = '';
+  public date: Date = new Date();
+  public isDate: boolean = false;
+  public amount: number = 0;
+
+  constructor() {}
+  onNameChange(event: any) {
+    console.log(event);
+    this.name = event.target.value;
+  }
+
+  onDateChange(event: any) {
+    this.isDate = true;
+    this.date = event.target.value;
+  }
+
+  onAmountChange(event: any) {
+    this.amount = parseFloat(event.target.value);
+  }
 }
